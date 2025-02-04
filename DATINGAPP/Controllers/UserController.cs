1187b2967ac11162116893d2f1e5b60caf1a9ad3
Md Ismail Hosen <md.ismail@brainstation-23.com>
2025-02-04 17:35:58 +0600

    added userApi

---
+++ DATINGAPP/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using DATINGAPP.Data;
+using DATINGAPP.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATINGAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UserController(DataContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await context.AppUsers.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUsersById(int id)
+        {
+            return await context.AppUsers.FindAsync(id);
+        }
+    }
+}
+
---
+++ DATINGAPP/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DATINGAPP.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATINGAPP.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
